e8efd797dbae6feb12df87e9265eb429b6c89289
Federico Roffi <fedoverseas@gmail.com>
2010-05-19 16:59:57 +0200

    Aggiornamenti e piccole correzioni.

--- src/java/it/unibo/cs/rgb/gwt/client/MainEntryPoint.java
+++ src/java/it/unibo/cs/rgb/gwt/client/MainEntryPoint.java
@@ -238,109 +238,6 @@ public class MainEntryPoint implements EntryPoint {
         //return new Label("Content");
     }
 
-    protected Widget createDoclistWidget() {
-
-        //TODO: da implementare l'rpc che richiede la lista dei files e quindi la generazione dinamica
-        class DoclistTree extends Composite implements TreeListener {
-
-            Tree tree = new Tree();
-            Label info = new Label("Doclist");
-
-            public DoclistTree() {
-                VerticalPanel vp = new VerticalPanel();
-                vp.addStyleName("demo-table");
-                vp.addStyleName("table-center");
-                vp.setWidth("150px");
-
-                TreeItem outerRoot = new TreeItem("Item 1");
-                outerRoot.addItem("Item 1-1");
-                outerRoot.addItem("Item 1-2");
-                outerRoot.addItem("Item 1-3");
-                tree.addItem(outerRoot);
-
-                TreeItem innerRoot = new TreeItem("Item 1-5");
-                innerRoot.addItem("Item 1-5-1");
-                innerRoot.addItem("Item 1-5-2");
-                innerRoot.addItem("Item 1-5-3");
-                innerRoot.addItem("Item 1-5-4");
-
-                outerRoot.addItem(innerRoot);
-                tree.addTreeListener(this);
-
-
-                vp.add(tree);
-                vp.setCellHeight(tree, "201px");
-
-                DOM.setStyleAttribute(info.getElement(),
-                        "backgroundColor", "#ffc");
-
-                DOM.setStyleAttribute(info.getElement(),
-                        "borderTop", "3px solid #999");
-
-                vp.add(info);
-
-                initWidget(vp);
-            }
-
-            public void onTreeItemSelected(TreeItem item) {
-                info.setText("Selected " + item.getText());
-
-                /*
-                 * if(item.getText() è in docviewPanel){
-                 *  trovala e selezionalo}
-                 * else{
-                 *  creane uno nuovo e selezionalo
-                 * }
-                 */
-                int index = docviewPanel.getWidgetCount()-1;
-
-                // cerco il tab
-                while(index >= 0){
-                    if(docviewPanel.getWidget(index).getTitle().equalsIgnoreCase(item.getText())){
-                        docviewPanel.selectTab(index);
-                        break;
-                    }
-                    index--;
-
-                }
-
-                // tab non esiste
-                if (index < 0) {
-
-                    // contenuto del tab
-                    final Widget tabPanel = createDocviewTab(item.getText());
-
-                    // tabText
-                    HorizontalPanel tabText = new HorizontalPanel();
-
-                    //TODO: visualizzare un tabText "carino"
-                    Label tabTitle = new Label("Tab " + item.getText());
-                    tabText.add(tabTitle);
-
-                    // X che chiude il tab
-                    ClickHandler xclose = new ClickHandler() {
-
-                        public void onClick(ClickEvent event) {
-                            docviewPanel.remove(tabPanel);
-                        }
-                    };
-                    Label x = new Label("X");
-                    x.addClickHandler(xclose);
-                    tabText.add(x);
-
-                    docviewPanel.add(tabPanel, tabText);
-                    docviewPanel.selectTab(docviewPanel.getWidgetCount()-1);
-                }
-            }
-
-            public void onTreeItemStateChanged(TreeItem item) {
-                info.setText(item.getText() + " State Changed");
-            }
-        }
-
-        return new DoclistTree();
-    }
-
     protected Widget createDocviewTab(String teiapath) {
         VerticalPanel panel = new VerticalPanel();
 
@@ -392,5 +289,4 @@ public class MainEntryPoint implements EntryPoint {
 
         return panel;
     }
-
 }
